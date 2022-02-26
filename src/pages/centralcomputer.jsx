8be437cefdae961d3eee1a4a07140d867afd7c1d
jmurphy5613 <jmurphy5613@gmail.com>
2022-02-26 11:04:31 -0500

    sync

--- src/pages/centralcomputer.jsx
+++ src/pages/centralcomputer.jsx
@@ -1,10 +1,23 @@
+import { makeStyles } from "@material-ui/core";
+import { Button } from '@material-ui/core';
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: theme.palette.primary.main
+    },
+
+}))
 
 
 const CentralComputer = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            fdhsajkfhjdaks
+        <div className={classes.root}>
+
         </div>
     )
 }
--- src/pages/scout.jsx
+++ src/pages/scout.jsx
@@ -1,10 +1,20 @@
+import { makeStyles } from "@material-ui/core";
 
-
+const useStyles = makeStyles( theme => ({
+    root: {
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: theme.palette.primary.main
+    }
+}));
 
 const Scout = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            fdklsajfldsa
+        <div className={classes.root}>
+            <a href="/">Home</a>
         </div>
     )
 }
