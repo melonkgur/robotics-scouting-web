1a2f564f717295139536a289d0a4d5fbfa6ad98f
jmurphy5613 <jmurphy5613@gmail.com>
2022-02-26 15:31:54 -0500

    set up qr scanner and popup on main computer

--- src/pages/centralcomputer.jsx
+++ src/pages/centralcomputer.jsx
@@ -1,8 +1,11 @@
 import { makeStyles } from "@material-ui/core";
 import { Button } from '@material-ui/core';
 import HomeButton from "../components/homebutton";
+import AddIcon from '@mui/icons-material/Add';
 import { useState } from "react";
 import QrReader from 'react-qr-scanner';
+import Popup from 'reactjs-popup';
+
 
 
 const useStyles = makeStyles(theme => ({
@@ -11,7 +14,21 @@ const useStyles = makeStyles(theme => ({
         height: '100vh',
         backgroundColor: theme.palette.primary.main
     },
-
+    navbarButtons: {
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        paddingTop: '2rem'
+    },
+    addDataButton: {
+        backgroundColor: '#32a852',
+        color: '#ffffff',
+    },
+    popupBackground: {
+        backgroundColor: '#000000b3',
+        height: '100vh',
+        width: '100vw'
+    }
 
 }))
 
@@ -20,13 +37,16 @@ const CentralComputer = () => {
 
     const classes = useStyles();
 
+
     const [qrData, setQrData] = useState({ test: 'lol' });
-    const [qrScanShow, setQrScanShow] = useState('none');
+    const [qrScanShow, setQrScanShow] = useState('flex');
 
     const previewStyle = {
         height: '700',
         width: '1000',
         display: `${qrScanShow}`,
+        margin: '0',
+        padding: '0',
     }
 
     const handleError = (err) => {
@@ -41,21 +61,34 @@ const CentralComputer = () => {
         console.log(qrData);
     }
 
-
     return (
         <div className={classes.root}>
             <HomeButton />
-            <Button onClick={e => {
-                setQrScanShow('block')
-            }}>Import QrCode</Button>
-            <QrReader
-                delay={1000}
-                onError={handleError}
-                onScan={handleScan}
-                style={previewStyle}
-            >
+            <div className={classes.navbarButtons}>
+                <Popup
+                    modal
+                    nested
+                    trigger={
+                        <Button
+                            variant="contained"
+                            onClick={e => {
+                                window.location = '/#/import-data'
+                            }}
+                            className={classes.addDataButton}
+                            endIcon={<AddIcon />}
+                        >Import QrCode</Button>
+                    }
+                >
 
-            </QrReader>
+                    {close => (
+                        <div className={classes.popupBackground}>
+                            <button onClick={close}>close</button>
+                        </div>
+                    )}
+
+                </Popup>
+
+            </div>
         </div>
     )
 }
