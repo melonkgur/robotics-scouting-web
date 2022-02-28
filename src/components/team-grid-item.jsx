39ffb4aa16d9568040974e313302e186a5079676
John Murphy <jmurphy5613@gmail.com>
2022-02-27 19:52:52 -0500

    set up for mondays meeting, waiting on qrcode styles to be resolved

---
+++ src/components/team-grid-item.jsx
@@ -0,0 +1,12 @@
+
+
+
+const TeamGridItem = (props) => {
+
+    return (
+        <div>
+            {/*some grid item here*/}
+            <h1>Team Grid</h1>
+        </div>
+    )
+}
---
+++ src/components/team-grid.jsx
@@ -0,0 +1,13 @@
+
+
+
+const TeamGrid = () => {
+    return (
+        <div>
+            {/*some grid system here to display team data*/}
+            <h1>Team Grid</h1>
+        </div>
+    )
+}
+
+export default TeamGrid;
