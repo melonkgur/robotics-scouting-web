5dbaf436029f061e37980abce07ff4aa933ed117
jmurphy5613 <jmurphy5613@gmail.com>
2022-03-09 08:52:25 -0500

    set up system for gettin averages

--- src/components/team-grid-item.jsx
+++ src/components/team-grid-item.jsx
@@ -1,10 +1,20 @@
-import { makeStyles } from "@material-ui/styles"
+import { makeStyles } from "@material-ui/styles";
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 
 const useStyles = makeStyles(theme => ({
     gridItem: {
-        height: '100px',
-        backgroundColor: '#ffffff'
+        height: '20vh',
+        backgroundColor: '#403e38',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        borderRadius: '1rem'
+    },
+    teamTitle: {
+        color: '#ffffff',
+        fontFamily: 'medium',
     }
 }))
 
@@ -12,9 +22,38 @@ const TeamGridItem = (props) => {
 
     const classes = useStyles();
 
+    const [teamData, setTeamData] = useState([])
+
+    useEffect(() => {
+        let teamData = JSON.parse(localStorage.getItem(`${props.team}`));
+        setTeamData(teamData);
+    }, []);
+
+    const getAverages = (teamData) => {
+        {console.log(teamData)}
+        let length = teamData.length;
+        let highGoalTotal = 0;
+        let lowGoalTotal = 0;
+
+        teamData.map(team => {
+            highGoalTotal += team.highGoalOperated;
+            lowGoalTotal += team.lowGoalOperated;
+        });
+        return {
+            highGoalAverage: highGoalTotal/length,
+            lowGoalAverage: lowGoalTotal/length
+        }
+    }
+
+
     return  (
         <div className={classes.gridItem}>
-            {console.log(props.team)}
+            <Typography variant="h3" className={classes.teamTitle}>
+                {props.team}
+            </Typography>
+            <Typography>
+                {getAverages(teamData).highGoalAverage}
+            </Typography>
         </div>
     )
 }
